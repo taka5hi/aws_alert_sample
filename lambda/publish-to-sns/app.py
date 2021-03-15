a8726527be6bd440569aac0afa140d3026c958fe
taka5hi <n.takashi930@gmail.com>
2021-03-15 11:39:38 +0900

    impl publish to sns

--- lambda/publish-to-sns/app.py
+++ lambda/publish-to-sns/app.py
@@ -1,2 +1,34 @@
-def handler(event, context):
-    print('hello, world!')
+import base64
+import json
+import zlib
+import datetime
+import os
+import boto3
+from botocore.exceptions import ClientError
+
+def lambda_handler(event, context):
+    data = zlib.decompress(base64.b64decode(event['awslogs']['data']), 16+zlib.MAX_WBITS)
+    data_json = json.loads(data)
+    log_json = json.loads(json.dumps(data_json["logEvents"][0], ensure_ascii=False))
+    
+    print(data_json)
+    log_group = data_json['logGroup']
+
+    print(log_json)
+    message = log_json['message']
+
+    try:
+        publish_to_sns(log_group, message)
+
+    except Exception as e:
+        print(e)
+        
+def publish_to_sns(subject, message):
+    sns = boto3.client('sns')
+
+    #SNS Publish
+    publishResponse = sns.publish(
+        TopicArn = os.environ['SNS_TOPIC_ARN'],
+        Message = message,
+        Subject = subject
+    )
